2e8adea52d1a06136b8278ed27483bf0fe4754b3
nikola <nikolaivkovic111@gmail.com>
2024-03-03 19:44:15 +0100

    work in progress

--- main.py
+++ main.py
@@ -2,18 +2,80 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import tensorflow as tfpip
-
-import logging
-tf.get_logger().setLevel(logging.ERROR) # da ne bi dolazilo do warning-a u izvrsavanju
-
+import tensorflow as tf
 from keras import layers
 from keras import Sequential
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import Dense
-from keras.losses import SparseCategoricalCrossentropy
+# from keras.losses import SparseCategoricalCrossentropy
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 
-# print('Hello world!')
+# avoid warnings when running
+import logging
+tf.get_logger().setLevel(logging.ERROR)
+
+# import training data
+dataset = pd.read_csv('Google_Stock_Price_Train.csv', index_col = 'Date', parse_dates = True)
+
+# import timestamps into data
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# create LSTM model
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+# import test data
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv', index_col = 'Date', parse_dates = True)
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# total dataset
+dataset_total = pd.concat((dataset['Open'], dataset_test['Open']), axis = 0)
+
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+
+inputs = sc.transform(inputs)
+
+X_test = []
+
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+    X_test = np.array(X_test)
+
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+# price prediction
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# result plotting
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
