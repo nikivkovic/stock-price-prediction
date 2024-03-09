2610f1fdcd3719990bba8c1903b16e165c24d6c4
nikolaivkovic <nikolaivkovic111@gmail.com>
2024-03-09 18:15:57 +0100

    work in progress

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ from keras.layers import Dense
 # from keras.losses import SparseCategoricalCrossentropy
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
 
 # avoid warnings when running
-import logging
-tf.get_logger().setLevel(logging.ERROR)
+# import logging
+# tf.get_logger().setLevel(logging.ERROR)
 
 # import training data
-dataset = pd.read_csv('Google_Stock_Price_Train.csv', index_col = 'Date', parse_dates = True)
+training_set = pd.read_csv('Google_Stock_Price_Train.csv', index_col = 'Date', parse_dates = True)
+
+# scaling
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
 
 # import timestamps into data
 X_train = []
@@ -46,6 +51,12 @@ regressor.add(Dropout(0.2))
 
 regressor.add(Dense(units = 1))
 
+# compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# fitting the RNN to the training set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
 # import test data
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv', index_col = 'Date', parse_dates = True)
 real_stock_price = dataset_test.iloc[:, 1:2].values
@@ -55,15 +66,13 @@ dataset_total = pd.concat((dataset['Open'], dataset_test['Open']), axis = 0)
 
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
-
 inputs = sc.transform(inputs)
 
 X_test = []
-
 for i in range(60, 80):
     X_test.append(inputs[i-60:i, 0])
-    X_test = np.array(X_test)
 
+X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 # price prediction
