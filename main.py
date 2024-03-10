c9e405cda972209aa290280d90ea3eb77b1b3347
nikola <nikolaivkovic111@gmail.com>
2024-03-10 20:25:52 +0100

    progress made: - datasets divided in train and test (check files) - support added for all datasets - code debugged for the BTC dataset, other datasets yet to be debugged - more work needed on plotting and on the rnn model parameters

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 # import packages
 import tensorflow as tf
 import logging
-
-# avoid warnings when running
-tf.get_logger().setLevel(logging.ERROR)
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,12 +13,35 @@ from keras.layers import Dropout
 from keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
 
+# avoid warnings when running
+tf.get_logger().setLevel(logging.ERROR)
+
 
 # Part 2: Import training dataset  ==================================================
 
 # import training data
 current_dir = os.path.dirname(os.path.abspath(__file__))
-dataset_training = pd.read_csv(os.path.join(current_dir, 'datasets', 'Google_Stock_Price_Train.csv'), index_col = 'Date', parse_dates = True)
+stock = 'BTC'
+
+if stock == 'BTC':
+    dataset_training = pd.read_csv(os.path.join(current_dir, 'datasets', 'BTC_Train.csv'), index_col = 'Date', parse_dates = True)
+    train_samples = 300
+    timestamp = 20
+elif stock == 'TSLA':
+    dataset_training = pd.read_csv(os.path.join(current_dir, 'datasets', 'TSLA_Train.csv'), index_col = 'Date', parse_dates = True)
+    train_samples = 200
+    timestamp = 40
+elif stock == 'S&P500':
+    dataset_training = pd.read_csv(os.path.join(current_dir, 'datasets', 'S&P500_Train.csv'), index_col = 'Date', parse_dates = True)
+    train_samples = 200
+    timestamp = 60
+else:
+    dataset_training = pd.read_csv(os.path.join(current_dir, 'datasets', 'GOOGL_Train.csv'), index_col = 'Date', parse_dates = True)
+    train_samples = 1258
+    timestamp = 60
+
+# convert columns from string to float
+dataset_training['Close'] = dataset_training['Close'].str.replace(',', '').astype(float)
 
 # preview imported dataset, check missing values and display info
 print('Training set ==========')
@@ -30,19 +49,15 @@ print(dataset_training.head())
 print(dataset_training.isna().any())
 dataset_training.info()
 
-# convert columns from string to float
-dataset_training['Close'] = dataset_training['Close'].str.replace(',', '').astype(float)
-dataset_training['Volume'] = dataset_training['Volume'].str.replace(',', '').astype(float)
-
 # preview dataset column which will be used for training
 plt.figure(1)
 dataset_training['Close'].plot(figsize = (16,6))
 plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
-plt.title('Google Stock Price')
+plt.ylabel(stock + ' Stock Price')
+plt.title(stock + ' Stock Price - Train Set')
 # plt.show()
 
-# use column Close for training only
+# use only column Close for training
 trainining_set = dataset_training['Close']
 trainining_set = pd.DataFrame(trainining_set)
 
@@ -53,8 +68,8 @@ trainining_set_scaled = sc.fit_transform(trainining_set)
 # import timestamps into data
 X_train = []
 y_train = []
-for i in range(50, 1258):
-    X_train.append(trainining_set_scaled[i-50:i, 0])
+for i in range(timestamp, train_samples):
+    X_train.append(trainining_set_scaled[i-timestamp:i, 0])
     y_train.append(trainining_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
@@ -81,19 +96,35 @@ regressor.add(Dropout(0.2))
 # adding output layer
 regressor.add(Dense(units = 1))
 
+# model summary
+regressor.summary()
+
 # compiling the RNN
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 # fitting the RNN to the training set
 # epochs = 100, batch_size = 32
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
 
 
 # Part 4: Making predictions and visualising the results ============================
 
 # import test data
-dataset_test = pd.read_csv(os.path.join(current_dir, 'datasets', 'Google_Stock_Price_Test.csv'), index_col = 'Date', parse_dates = True)
-dataset_test['Volume'] = dataset_test['Volume'].str.replace(',', '').astype(float)
+if stock == 'BTC':
+    dataset_test = pd.read_csv(os.path.join(current_dir, 'datasets', 'BTC_Test.csv'), index_col = 'Date', parse_dates = True)
+    test_samples = 67
+elif stock == 'TSLA':
+    dataset_test = pd.read_csv(os.path.join(current_dir, 'datasets', 'TSLA_Test.csv'), index_col = 'Date', parse_dates = True)
+    test_samples = 53
+elif stock == 'S&P500':
+    dataset_test = pd.read_csv(os.path.join(current_dir, 'datasets', 'S&P500_Test.csv'), index_col = 'Date', parse_dates = True)
+    test_samples = 53
+else:
+    dataset_test = pd.read_csv(os.path.join(current_dir, 'datasets', 'GOOGL_Test.csv'), index_col = 'Date', parse_dates = True)
+    test_samples = 20
+
+# convert columns from string to float
+dataset_test['Close'] = dataset_test['Close'].str.replace(',', '').astype(float)
 
 # preview imported dataset, check missing values and display info
 print('Testing set ==========')
@@ -101,22 +132,32 @@ print(dataset_test.head())
 print(dataset_test.isna().any())
 dataset_test.info()
 
-real_stock_price = dataset_test.iloc[:, 1:2].values
+# preview dataset column which will be used for training
+plt.figure(2)
+dataset_test['Close'].plot(figsize = (16,6))
+plt.xlabel('Time')
+plt.ylabel(stock + ' Stock Price')
+plt.title(stock + ' Stock Price - Test Set')
+# plt.show()
 
-# use column Close for test only
+# get the real stock price (from test dataset)
+real_stock_price = dataset_test.iloc[:, 3:4].values
+print(real_stock_price.shape)
+
+# use only column Close for the test
 test_set = dataset_test['Close']
 test_set = pd.DataFrame(test_set)
 
 # total dataset
 dataset_total = pd.concat((dataset_training['Close'], dataset_test['Close']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 200:].values
-inputs = inputs.reshape(-1,1)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - timestamp:].values
+inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
 
 # import timestamps into data
 X_test = []
-for i in range(50, 70):
-    X_test.append(inputs[i-50:i, 0])
+for i in range(timestamp, timestamp + test_samples):
+    X_test.append(inputs[i-timestamp:i, 0])
 X_test = np.array(X_test)
 
 # reshaping
@@ -128,26 +169,35 @@ predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 predicted_stock_price = pd.DataFrame(predicted_stock_price)
 
 # prediction result plotting
-plt.figure(2)
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'red', label = 'Predicted Google Stock Price')
+plt.figure(3)
+plt.plot(real_stock_price, color = 'red', label = 'Real ' + stock + ' Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted ' + stock + ' Stock Price')
 plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
-plt.title('Google Stock Price Prediction')
+plt.ylabel(stock + ' Stock Price')
+plt.title(stock + ' Stock Price Prediction')
 plt.legend()
 
+# get the real stock price (from total dataset)
 real_stock_price = dataset_total.values
 
+# add NaN values before the predicted values
 temp = predicted_stock_price.values
-predicted_stock_pric = np.full((1278,1), np.nan)
-predicted_stock_price[1258:,] = tmep
+predicted_stock_price = np.full((train_samples + test_samples,1), np.nan)
+predicted_stock_price[train_samples:,] = temp
 
 # prediction result plotting in wider context
-plt.figure(3)
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'red', label = 'Predicted Google Stock Price')
+plt.figure(4)
+plt.plot(real_stock_price, color = 'red', label = 'Real ' + stock + ' Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted ' + stock + ' Stock Price')
 plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
-plt.title('Google Stock Price Prediction')
+plt.ylabel(stock + ' Stock Price')
+plt.title(stock + ' Stock Price Prediction')
 plt.legend()
 plt.show()
+
+# TO-DO:
+#TODO: check nmm_cnn.main.py file and look at the compilation of the nn
+#TODO: add date in the results plot instead of samples (x-axis)?
+#TODO: add timestamp value to legend?
+#TODO: add vertical line where the prediction starts on the wider context chart?
+#TODO: check other stocks and play with parameters
